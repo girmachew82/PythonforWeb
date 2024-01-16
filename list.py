71651215e204d2f49d95d16f903217c058e39f9f
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2024-01-16 12:45:06 +0300

    List

---
+++ list.py
@@ -0,0 +1,11 @@
+print("Before sort")
+names = [ "Kebede", "Abebe","Lemma"]
+print(names)
+print("After sort")
+names.sort()
+print(names)
+print("Append name ")
+
+names.append("Tefera")
+print("After append")
+print(names)

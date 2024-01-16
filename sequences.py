ec0381daa6fd1932421990574b517e09ad90de9f
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2024-01-16 12:39:20 +0300

    sequences

---
+++ sequences.py
@@ -0,0 +1,14 @@
+#first sequence string
+name = "Aebeb"
+print(name[0])
+#second sequence list //mutable
+names = ["Abebe", "Kebede", "Lemma"]
+print(names)
+print(names[2])
+#third sequence tuple //immutable
+coordinats = (10.0, 20.0)
+#fouth sequence set // 
+
+
+#fiveth sequence dict // key-value pairs
+
